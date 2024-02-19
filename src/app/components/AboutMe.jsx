9de583c5c8c53233443aeffd50fb2502f95e8fc1
Kerry Creech <kerrywcreech@gmail.com>
2024-02-18 23:10:16 -0500

    Updated About Me Section

--- src/app/components/AboutMe.jsx
+++ src/app/components/AboutMe.jsx
@@ -10,6 +10,13 @@ const AboutMe = () => {
           </span>
         </h1>
         <div>
+            <Image 
+                src="/images/nypic.jpg"
+                alt="Picture of me in Central Park in NYC"
+                className="flex mx-auto justify-center mb-10"
+                height={300}
+                width={300}
+            />
             <p className="text-[#d3d3d3] text-lg lg:text-xl mb-7 leading-loose lg:leading-loose">
                 Hi! I'm from Charlotte, NC, and a student at UNC Chapel Hill.
                 At UNC, I am involved with organizations including TechX, CS+SG, and the Asian American Students Association.
@@ -25,13 +32,14 @@ const AboutMe = () => {
                 height={300}
                 width={300}
             />
+            <p className="text-white text-center text-sm mt-2">Me competing at CubingUSA Nationals 2023 in Pittsburgh, PA</p>
         </div>
         <div>
-        <p className="text-[#d3d3d3] text-lg lg:text-xl mt-7 leading-loose lg:leading-loose">
-            My passion for Computer Science began in the 7th grade, when I joined my school's coding club. There, I learned the basics of frontend
+        <p className="text-[#d3d3d3] text-lg lg:text-xl mt-7 mb-20 leading-loose lg:leading-loose">
+            My love for Computer Science began in the 7th grade, when I joined my school's coding club. There, I learned the basics of frontend
             development with HTML and CSS. I immediately began creating websites about anything I could think of.
             <br></br>Later on, in high school, I took CS courses through dual enrollment, and learned a lot about Java and backend development in general.
-            Now, here at UNC, being a part of organizations like TechX allows me to get involved with projects that will further enhance my technical skills.
+            Now, here at UNC, I am involved with a couple projects that will further enhance my technical skills.
         </p>
         </div>
     </div>
